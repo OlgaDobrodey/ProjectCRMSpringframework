94e312ced20b6df70410061eb24a56be53eb5bea
OlgaDobrodey <olga.gurkovskaya@gmail.com>
2021-11-21 12:29:15 +0300

    first commit

---
+++ src/main/java/com/itrex/java/lab/projectcrmspringboot/ProjectCrmSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.itrex.java.lab.projectcrmspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCrmSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCrmSpringBootApplication.class, args);
+    }
+
+}
